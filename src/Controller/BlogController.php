3d75a37ec68baea3bf7d1574c7d63826224423ec
francois-niang1 <francois.niang@laplateforme.io>
2022-09-25 20:26:16 +0200

    add comment check + display info user

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Articles;
+use App\Entity\Commentaires;
+use App\Form\CommentType;
 use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,8 +27,26 @@ class BlogController extends AbstractController
     }
 
     #[Route('/blog/detail/{id}', name: 'detail')]
-    public function SingleArticle(Articles $article): Response
+    public function SingleArticle(Request $request, EntityManagerInterface $entityManager, Articles $article): Response
     {
-        return $this->render('blog/detail.html.twig', compact('article'));
+        $comment = new Commentaires();
+        $comment -> setDate(new \DateTime());
+        $User = $this->getUser();
+        $comment->setUser($User);
+        $comment->setArticle($article);
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        dump($article);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+        }
+
+        return $this->render('blog/detail.html.twig', [
+            'commentForm' => $form->createView(),
+            'article' => $article
+        ]);
     }
 }
---
+++ src/Form/CommentType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaires;
+use DateTime;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('texte', TextareaType::class, [
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaires::class,
+        ]);
+    }
+}
